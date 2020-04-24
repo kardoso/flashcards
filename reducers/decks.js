84b5053042d94ce83513aa5b93238308c7fdd9d9
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-24 12:17:37 -0300

    fix: Fix reducer to delete deck

--- reducers/decks.js
+++ reducers/decks.js
@@ -1,5 +1,11 @@
 import { RECEIVE_DECKS, ADD_DECK, REMOVE_DECK } from '../actions/decks'
 
+function removeKey(obj, deleteKey) {
+  let clone = Object.assign({}, obj)
+  delete clone[deleteKey]
+  return clone
+}
+
 export default function decks(state = {}, action) {
   switch (action.type) {
     case RECEIVE_DECKS:
@@ -13,10 +19,7 @@ export default function decks(state = {}, action) {
         [action.deck.id]: action.deck,
       }
     case REMOVE_DECK:
-      return {
-        [action.deck.id]: omit,
-        ...state,
-      }
+      return removeKey(state.decks, action.id)
     default:
       return state
   }
